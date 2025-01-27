75f515dc716799d960987490cca81e75cb0c0458
Eelant <luonglkvn100@gmail.com>
2025-01-27 12:59:33 +0700

    [Update] 🔨 apply new URI

--- app/core/component/header/header.tsx
+++ app/core/component/header/header.tsx
@@ -147,7 +147,7 @@ export function Header({header_lang, lang_func, language} : {header_lang : TFunc
             <div className="">
               <Link
                 style={{ padding: 10 }}
-                href="https://drive.usercontent.google.com/uc?id=17mAOJzJ5REG6AnydFR3YB1nRRRlRD8af&authuser=0&export=download"
+                href="https://drive.usercontent.google.com/uc?id=1PlEpXbbQ9YSSJkMGAt4zDxlfvS61tERg&authuser=0&export=download"
                 download
               >
                 {header_lang("common:cv")}
